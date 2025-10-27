1e3beb019100be6090905b405c172ca38b746f05
BhavneetSingh <bsingh@eseye.com>
2025-10-27 11:06:07 +0530

    wip'

--- api/main.py
+++ api/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
-from typing import Union
+from fastapi import FastAPI, UploadFile, Response
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/heartbeat")
 async def heartbeat():
     return "pumping"
-
 
 @app.post("/publish")
 async def publish_video(file: UploadFile):
@@ -15,7 +13,30 @@ async def publish_video(file: UploadFile):
         f.write(contents.decode(encoding="utf-8"))
     return {"published": file}
 
+@app.get("/video/{file:path}")
+def hls_files(file: str):
+    contents = bytes()
+    try:
+        with open(f"./storage/{str(file)}", "rb") as f:
+            contents = f.read()
+    except FileNotFoundError:
+        return Response({"msg": "file not found"}, media_type="application/json", status_code=404, headers={"Access-Control-Allow-Origin": "*"})
 
-@app.get("/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+    return Response(
+        contents, media_type="application/x-mpegURL",
+        headers={"Access-Control-Allow-Origin": "*"}
+    )
+
+@app.get("/{file:path}")
+def static_files(file: str):
+    contents = bytes()
+    try:
+        with open(f"./app/{str(file)}", "rt") as f:
+            contents = f.read()
+    except Exception:
+        return Response({"msg": "file not found"}, media_type="application/json", status_code=404, headers={"Access-Control-Allow-Origin": "*"})
+
+    return Response(
+        contents, headers={"Access-Control-Allow-Origin": "*"}
+    )
+
